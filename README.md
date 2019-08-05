132c5f27819904f40d5853987b48c2b34052da1c
Daniel Corcoran <danielc.92@outlook.com>
2019-08-05 18:55:56 +1000

    Added middleware

---
+++ README.md
@@ -0,0 +1,1 @@
+# GraphQL Javascript Server
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
+const graphqlHTTP = require('express-graphql');
 const app = express();
 
+// Use the graphql as middleware
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 const port = 3001;
 app.listen(port, ()=>{ console.log(`listening for requests on port ${port}`)})
