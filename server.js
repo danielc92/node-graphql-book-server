a1212608fcbe262b7491f179cefc1430809d8776
Daniel Corcoran <danielc.92@outlook.com>
2019-08-06 08:56:36 +1000

    Added mongoose and connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
+
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017');
+mongoose.connection.once('open', ()=>{
+    console.log('The connection is open.')
+})
 
 
 // Use the graphql as middleware
